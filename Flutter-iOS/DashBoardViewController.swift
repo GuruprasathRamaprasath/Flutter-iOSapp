0ca9d97dbd48909cad3c0db72c5971b0d9a83a8f
Guruprasath <guru.rit@gmail.com>
2019-01-09 15:51:08 +0530

    PagerView Implemented

---
+++ Flutter-iOS/DashBoardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashBoardViewController.swift
+//  Flutter-iOS
+//
+//  Created by Guruprasath on 09/01/19.
+//  Copyright © 2019 Guruprasath. All rights reserved.
+//
+
+import UIKit
+
+class DashBoardViewController: FlutterViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
